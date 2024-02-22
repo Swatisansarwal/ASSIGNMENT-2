935b15904d9ca9400cab955377ad6986883e63b2
Swatisansarwal <156865650+Swatisansarwal@users.noreply.github.com>
2024-02-23 04:00:54 +0530

    edit printing statement

--- Program.cs
+++ Program.cs
@@ -209,14 +209,14 @@ public class Board
                 newY++;
                 break;
             default:
-                Console.WriteLine("Invalid direction");
+                Console.WriteLine("The direction is invalid");
                 return; // Exit the method if the direction is invalid
         }
 
         // Check if the new position is within the bounds of the board
         if (newX < 0 || newX >= 6 || newY < 0 || newY >= 6)
         {
-            Console.WriteLine("Cannot move outside the board.");
+            Console.WriteLine(" Player Cannot move outside the board.");
             return; // Exit the method if the move is outside the board
         }
 
